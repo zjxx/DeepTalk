02de57144787cc24a271f7d16b97bc330b23958b
zjxx <204876260@qq.com>
2025-05-09 11:32:27 +0800

    init:登录功能

---
+++ src/main/java/com/example/loginsystem/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.loginsystem.entity;
+
+import lombok.Data;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String role = "USER";
+} 
