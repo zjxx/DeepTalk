41f76b4dc86b18eb92976d9099b10e87e6bd54f9
yan_tai <213221537@seu.edu.cn>
2025-06-04 19:40:38 +0800

    temp:回滚了https

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +11,10 @@ export default defineConfig({
     vue(),
     eslint(),
     VueDevTools(),
-    basicSsl(),
   ],
   server: {
     host: '0.0.0.0',
     port: 5173,
-    https: {}
   },
   resolve: {
     alias: {
