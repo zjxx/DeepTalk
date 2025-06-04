cd5c04ec1892e91065a583f44e601bcdb8f07dc2
yan_tai <213221537@seu.edu.cn>
2025-06-04 13:41:49 +0800

    feat:局域网访问

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
     eslint(),
     VueDevTools(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
