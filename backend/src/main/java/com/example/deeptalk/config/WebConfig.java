c1d077549377db9abc72dec3ec354105266fd46b
Pu Jing <ChPu437@seu.edu.cn>
2025-06-07 17:05:22 +0800

    fix: 更新测试设置，使得Websocket消息大小限制可以通过测试

--- backend/src/main/java/com/example/deeptalk/config/WebConfig.java
+++ backend/src/main/java/com/example/deeptalk/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.example.deeptalk.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -38,6 +39,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     // Config for max WebSocket message buffer size
     @Bean
+    @Profile("!test") // 仅在非测试环境中生效
     public ServletServerContainerFactoryBean createWebSocketContainer() {
         ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
 
