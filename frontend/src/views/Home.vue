48174407a99ab877c13e43dffe35a3c8c0299212
chen-symx <1574810719@qq.com>
2025-06-08 02:27:30 +0800

    feat：添加一个近期评分表格

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="home">
-    <TodoList />
+    <v-container fluid>
+      <v-row>
+        <!-- 左侧待办列表 -->
+        <v-col cols="12" md="6">
+          <TodoList />
+        </v-col>
+        
+        <!-- 右侧评分图表 -->
+        <v-col cols="12" md="6">
+          <ScoreChart />
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
 import TodoList from '../components/TodoList.vue'
+import ScoreChart from '../components/ScoreChart.vue'
 </script>
 
 <style scoped>
 .home {
   padding: 20px;
+  background-color: #f5f5f5;
+  min-height: 100vh;
 }
 </style>
